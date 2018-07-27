faeb9fca37ffbb61ac8ff7b914455ee4c0c74fd6
Richard Griffiths <cwolfs@gmail.com>
2018-07-27 09:47:24 +0200

    Fix tabs/spaces

--- src/Core/ConversationUpgrades.cs
+++ src/Core/ConversationUpgrades.cs
@@ -21,19 +21,21 @@ namespace ExtendedConversations.Core {
 
       Main.Logger.Log("Declaring 'Evaluate Tag for Current System' condition operation");
       TsOp tsOp = env.DeclareOp("ConditionFunction", "Evaluate Tag for Current System", boolType, new TsOp.EvalDelegate(ConversationUpgrades.EvaluateTagForCurrentSystem));
-			tsOp.DeclareInput("comp", HasOrHasNotType);
-			tsOp.DeclareInput("tagName", SenseTagListType);
+      tsOp.DeclareInput("comp", HasOrHasNotType);
+      tsOp.DeclareInput("tagName", SenseTagListType);
 
       Main.Logger.Log("Finished declaring conversation upgrades");
     }
 
     /* OPERATIONS */
     public static object EvaluateTagForCurrentSystem(TsEnvironment env, object[] inputs) {
-			bool flag = env.ToBool(inputs[0]);
-			string value = env.ToString(inputs[1]);
-			TagSet currentSystemTags = UnityGameInstance.BattleTechGame.Simulation.CurSystem.Tags;
-			bool flag2 = currentSystemTags.Contains(value) == flag;
-			return flag2;
+      Main.Logger.Log("EvaluateTagForCurrentSystem triggered");
+      bool flag = env.ToBool(inputs[0]);
+      string value = env.ToString(inputs[1]);
+      TagSet currentSystemTags = UnityGameInstance.BattleTechGame.Simulation.CurSystem.Tags;
+      bool flag2 = currentSystemTags.Contains(value) == flag;
+      Main.Logger.Log("EvaluateTagForCurrentSystem finished with result of " + flag2);
+      return flag2;
 		}
   }
 }
