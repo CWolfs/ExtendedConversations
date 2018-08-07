ff776f327422b85ba7a388287adec5a561b7a5ea
Richard Griffiths <cwolfs@gmail.com>
2018-08-07 10:37:40 +0200

    Added `TimeSkip` action & refactored actions/conditions

---
+++ src/Util/ReflectionHelper.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Reflection;
+
+namespace ExtendedConversations.Utils {
+    public static class ReflectionHelper {
+        public static object InvokePrivateMethod(object instance, string methodname, object[] parameters) {
+            Type type = instance.GetType();
+            MethodInfo methodInfo = type.GetMethod(methodname, BindingFlags.NonPublic | BindingFlags.Instance);
+            return methodInfo.Invoke(instance, parameters);
+        }
+
+        public static object InvokePrivateMethod(object instance, string methodname, object[] parameters, Type[] types) {
+            Type type = instance.GetType();
+            MethodInfo methodInfo = type.GetMethod(methodname, BindingFlags.NonPublic | BindingFlags.Instance, null, types, null);
+            return methodInfo.Invoke(instance, parameters);
+        }
+
+        public static void SetPrivateProperty(object instance, string propertyname, object value) {
+            Type type = instance.GetType();
+            PropertyInfo property = type.GetProperty(propertyname, BindingFlags.NonPublic | BindingFlags.Instance);
+            property.SetValue(instance, value, null);
+        }
+
+        public static void SetPrivateField(object instance, string fieldname, object value) {
+            Type type = instance.GetType();
+            FieldInfo field = type.GetField(fieldname, BindingFlags.NonPublic | BindingFlags.Instance);
+            field.SetValue(instance, value);
+        }
+
+        public static object GetPrivateField(object instance, string fieldname) {
+            Type type = instance.GetType();
+            FieldInfo field = type.GetField(fieldname, BindingFlags.NonPublic | BindingFlags.Instance);
+            return field.GetValue(instance);
+        }
+
+        public static object GetPrivateStaticField(Type instance, string fieldname) {
+            FieldInfo field = instance.GetField(fieldname, BindingFlags.NonPublic | BindingFlags.Static);
+            return field.GetValue(instance);
+        }
+    }
+}
