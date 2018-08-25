74e1ec39ff0f662027fe70bdb59ee16082800383
Richard Griffiths <cwolfs@gmail.com>
2018-08-25 09:28:04 +0200

    Use .net 3.5 and change interpolator for furture proofing

--- src/Core/ConversationInterpolator.cs
+++ src/Core/ConversationInterpolator.cs
@@ -19,15 +19,6 @@ namespace ExtendedConversations.Core {
     }
 
     public string Interpolate(string text) {
-      string interpolatedText = this.InterpolateStats(text);
-
-      return interpolatedText;
-    }
-
-    /*
-      Looks up any stats element and replaces that over the placeholder
-    */
-    private string InterpolateStats(string text) {
       Main.Logger.Log($"[InterpolateStats] text '{text}'");
 
 			Regex regex = new Regex("\\<.*?\\>");
@@ -42,28 +33,43 @@ namespace ExtendedConversations.Core {
 				int index = match.Index;
 				int length = match.Length;
 
-        // FORMAT: CI.[StatType].[StatKey]
         string[] lookups = expression.Split('.');
-        if (lookups.Length < 3) {
-          Main.Logger.LogError($"[InterpolateStats] {{Expression}} needs to be in the format of {{CI.StatType.StatKey}}");
-          return $"[InterpolateStats] {{Expression}} needs to be in the format of {{CI.StatType.StatKey}}";
-        }
+        string actionType = lookups[0].ToLower();
 
-        string statType = lookups[1];
-        string statName = lookups[2];
-        StatCollection statCollection = SimHelper.GetStatCollection(statType);
-
-        if (statCollection.ContainsStatistic(statName)) {
-          string statValue = statCollection.GetValue<string>(statName);
-          text = text.Remove(index, length).Insert(index, statValue);
-          matchCollection = regex.Matches(text);
+        string interpolatedText = "";
+        if (actionType.ToLower() == "stats") {
+          interpolatedText = this.InterpolateStats(text, lookups);
         } else {
-          Main.Logger.LogError($"[InterpolateStats] Stat '{statName}' does not exist for stat collection '{statType}'");
-          return $"[InterpolateStats] Stat {statName} does not exist for stat collection {statType}";
+          Main.Logger.LogError($"[InterpolateStats] Unknown interpolation action of {actionType}");
         }
-			}
+
+        text = text.Remove(index, length).Insert(index, interpolatedText);
+        matchCollection = regex.Matches(text);
+      }
 
       return text;
+    }
+
+    /*
+      Looks up any stats element and replaces that over the placeholder
+    */
+    private string InterpolateStats(string text, string[] lookups) {
+      if (lookups.Length < 3) {
+        Main.Logger.LogError($"[InterpolateStats] {{Expression}} needs to be in the format of {{CI.StatType.StatKey}}");
+        return $"[InterpolateStats] {{Expression}} needs to be in the format of {{CI.StatType.StatKey}}";
+      }
+
+      string statType = lookups[1];
+      string statName = lookups[2];
+      StatCollection statCollection = SimHelper.GetStatCollection(statType);
+
+      if (statCollection.ContainsStatistic(statName)) {
+        string statValue = statCollection.GetValue<string>(statName);
+        return statValue;
+      } else {
+        Main.Logger.LogError($"[InterpolateStats] Stat '{statName}' does not exist for stat collection '{statType}'");
+        return $"[InterpolateStats] Stat {statName} does not exist for stat collection {statType}";
+      }	
     }
   }
 }
