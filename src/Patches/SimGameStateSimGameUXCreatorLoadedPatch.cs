7da5df17af400407023b473fa64d9de6858f594b
Richard Griffiths <cwolfs@gmail.com>
2023-06-14 11:18:02 +0200

    Modified patch point

--- src/Patches/SimGameStateSimGameUXCreatorLoadedPatch.cs
+++ src/Patches/SimGameStateSimGameUXCreatorLoadedPatch.cs
@@ -5,8 +5,8 @@ using BattleTech;
 using ExtendedConversations.Animation;
 
 namespace ExtendedConversations {
-  [HarmonyPatch(typeof(SimGameState), "OnHeadAttachedStateCompleteListener")]
-  public class SimGameStateOnHeadAttachedStateCompleteListenerPatch {
+  [HarmonyPatch(typeof(SimGameState), "SimGameUXCreatorLoaded")]
+  public class SimGameStateSimGameUXCreatorLoadedPatch {
     public static void Postfix(SimGameState __instance) {
       AnimationManager.Instance.ReplaceAllVanillaCrewWithMimics();
     }
